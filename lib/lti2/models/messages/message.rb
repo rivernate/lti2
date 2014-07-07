4d3ca6dd305d78236a3ffb56b2d91d5accbb3a8e
Nathan Mills <nathanm@instructure.com>
2014-07-07 14:13:49 -0600

    add custom and ext params to messages

--- lib/lti2/models/messages/message.rb
+++ lib/lti2/models/messages/message.rb
@@ -7,8 +7,36 @@ module LTI2::Models::Messages
                    :launch_presentation_document_target, :launch_presentation_css_url, :launch_presentation_width,
                    :launch_presentation_height
 
-    def post_params
-      as_json
+    def initialize
+      @custom_params = {}
+      @ext_params = {}
     end
+
+    def post_params
+      get_custom_params.merge(get_ext_params).merge(attributes)
+    end
+
+    def get_ext_params
+      params = {}
+      @ext_params.each { |k, v| params["ext_#{k}"] = v }
+      params
+    end
+
+    def get_custom_params
+      params = {}
+      @custom_params.each { |k, v| params["custom_#{k}"] = v }
+      params
+    end
+
+    def method_missing(meth, *args, &block)
+      if match = /^(custom|ext)_([^=$]*)/.match(meth)
+        param_type, key = match.captures
+        param_hash = instance_variable_get("@#{param_type}_params".to_sym)
+        meth =~ /=$/ ? param_hash[key] = args[0] : param_hash[key]
+      else
+        super
+      end
+    end
+
   end
 end
--- spec/lti2/models/messages/message_spec.rb
+++ spec/lti2/models/messages/message_spec.rb
@@ -10,5 +10,42 @@ module LTI2::Models::Messages
       expect(params['lti_message_type']).to eq('message-type')
       expect(params['user_id']).to eq('123456')
     end
+
+    it 'returns custom params for the message' do
+      subject.custom_name = 'my_custom_name'
+
+      params = subject.post_params
+      expect(params['custom_name']).to eq('my_custom_name')
+    end
+
+    it 'returns ext params for the message' do
+      subject.ext_name = 'my_ext_name'
+
+      params = subject.post_params
+      expect(params['ext_name']).to eq('my_ext_name')
+    end
+
+    it 'sets a ext param' do
+      subject.ext_name = 'my_ext_name'
+      expect(subject.ext_name).to eq 'my_ext_name'
+    end
+
+    it 'sets an custom param' do
+      subject.custom_name = 'my_custom_name'
+      expect(subject.custom_name).to eq 'my_custom_name'
+    end
+
+    it 'returns all the custom params' do
+      subject.custom_name = 'my_custom_name'
+      subject.custom_number = '3'
+      expect(subject.get_custom_params).to eq({'custom_name' => 'my_custom_name', 'custom_number' => '3'})
+    end
+
+    it 'returns all the ext params' do
+      subject.ext_name = 'my_ext_name'
+      subject.ext_number = '4'
+      expect(subject.get_ext_params).to eq({'ext_name' => 'my_ext_name', 'ext_number' => '4'})
+    end
+
   end
 end
